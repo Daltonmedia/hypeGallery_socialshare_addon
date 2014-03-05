4354e3cd69b40688a4aad6f7a3c590b189cb7f41
Dries <driesdk@gmail.com>
2014-03-05 12:35:53 +0100

    Update start.php

--- start.php
+++ start.php
@@ -4,7 +4,7 @@ elgg_register_event_handler('init', 'system', 'hypegallery_socialshare_addon');
     function hypegallery_socialshare_addon()
     {
 
-    elgg_extend_view('object/hjalbumimage/meta', 'social/extend', 1);
-    elgg_extend_view('css/elgg', 'css/social/cssextend');
+    elgg_extend_view('object/hjalbumimage/meta', 'hypegallery_socialshare/extend', 1);
+    elgg_extend_view('css/elgg', 'css/hypegallery_socialshare/cssextend');
 }
 ?>
